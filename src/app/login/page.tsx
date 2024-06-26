00e6cf1d7aea5ab6f6889bacc269807b046e1b0e
Ho Tan Hung <tanhungho2002@gmail.com>
2024-06-26 18:46:37 +0700

    update deploy

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,25 +5,29 @@ import LoginButton from "@/components/ButtonLogin";
 interface Props {
   providers: Record<string, ClientSafeProvider>;
 }
-// export async function getServerSideProps() {
-//   const providers = await getProviders();
 
-//   return {
-//     props: {
-//       providers,
-//     },
-//   };
-// }
 async function Login() {
-  const providers = await getProviders();
-  const { name, id } = providers?.spotify as ClientSafeProvider;
+  try {
+    const providers = await getProviders();
+    const { name, id } = providers?.spotify as ClientSafeProvider;
 
-  return (
-    <div className="flex flex-col justify-center items-center bg-black h-screen">
-      <Image src={spotifyLogo} alt="SpotifyLogo" height="200" width="200" />
-      <LoginButton providerId={id} providerName={name} />
-    </div>
-  );
+    if (!name || !id) {
+      throw new Error("Spotify provider not found");
+    }
+
+    return (
+      <div className="flex flex-col justify-center items-center bg-black h-screen">
+        <Image src={spotifyLogo} alt="SpotifyLogo" height="200" width="200" />
+        <LoginButton providerId={id} providerName={name} />
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching providers:", error);
+    // Handle error display or fallback
+    return (
+      <div>An error occurred while fetching authentication providers.</div>
+    );
+  }
 }
 
 export default Login;
